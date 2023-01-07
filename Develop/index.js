e3ecaae0c9e921dc066b1b0748fcd883a9fe15f9
JENNIFER SCHERSCHLIGT <patinopher@gmail.com>
2023-01-07 14:25:37 -0800

    cleaned up code.

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,105 @@
-// TODO: Include packages needed for this application
+//Packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+//Create an array of questions for user input
+//const questions = [];
+inquirer
+    .prompt ([
+        {
+            type: "input",
+            message: "What is the title of your project? (Required)",
+            name: "title", 
+            validate: titleInput => {
+                if(titleInput) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }  
+        },
+        {
+            type: "input",
+            message: "Why did you build this project?",
+            name: "why",
+        },
+        {
+            type: "input",
+            message: "What problem does it solve?",
+            name: "problem",
+        },
+        {
+            type: "input",
+            message: "What did you learn?",
+            name: "learn",
+        },
+        {
+            type: "input",
+            message: "How can a user install this application?",
+            name: "install",
+        },
+        {
+            type: "input",
+            message: "Add a screenshot using ![alt text](.png)",
+            name: "screenshot",
+        },
+        {
+            type: "input",
+            message: "What are the key features of the application?",
+            name: "features",
+        },
+        {
+            type: "input",
+            message: "List all collaborators and links to their Github Profile",
+            name: "contributors",
+        },
+        {
+            type: "input",
+            message: "Were any third-party assets or tutorials used that require attribution?",
+            name: "thirdparty",
+        },
+        {
+            type: "input",
+            message: "How can a user test the application?",
+            name: "test",
+        },
+        {
+            type: "list",
+            message: "Choose a license for the application",
+            name: "license",
+            choices: ['Unlicense', 'Apache', 'GNU General Public License', 'MIT License' ]
+        },
+        {
+            type: "input",
+            message: "How can other developers contribute?",
+            name: "contribute",
+        },
+        {
+            type: "input",
+            message: "Email address:",
+            name: "email",
+        },
+        {
+            type: "input",
+            message: "Github username:",
+            name: "github",
+        }
+    ]).then((data) => {
+        console.log(data);
+        fs.writeFile('README.md', generateMarkdown(data),
+        error => {
+            if (error) {
+                console.log('Input all the data')
+            }
+            console.log('Your README was generated')
+        }
+        )
+    })
 
-// TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
+// Function to initialize app
 function init() {}
 
 // Function call to initialize app
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,72 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicense(license) {
+  if (license === 'Unlicense') {
+    return `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`
+  }
+  if (license === 'Apache') {
+    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+  }
+  if (license === 'GNU General Public License') {
+    return `[![License: GNU General Public License](https://www.gnu.org/graphics/gplv3-127x51.png)](https://www.gnu.org/licenses/gpl-3.0)`
+  }
+  if (license === 'MIT License') {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+}
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
+// Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+## Description
+
+  ${data.why}
+  ${data.problem} 
+  ${data.learn}
+
+ ## Table of Contents
+
+ - [License](#license)
+ - [Installation](#installation)
+ - [Usage](#usage)
+ - [Features](#features)
+ - [Credits](#contributors--credit)
+ - [Tests](#tests)
+ - [Questions](#questions)
+
+ ## Installation
+
+To install this application, follow these steps:
+ ${data.install}
+
+## Usage
+
+## License
+
+ ${renderLicense(data.license)} 
+
+## Contributors & Credit
+
+ ${data.contributors}
+ ${data.thirdparty}
+
+## Tests
+
+${data.test}
+
+## Questions
+ Please email me, if you have any questions
+${data.email}, or visit my GitHub account at
+[${data.github}](https://github.com/${data.github})
+
+## Features
+
+ ${data.features}
+
+## How to Contribute
+
+ ${data.contribute}
 
 `;
 }
